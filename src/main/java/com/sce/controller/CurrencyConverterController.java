c0dcae2c2f1ac58cf496af54bc1abfc931dcbd77
Aanchal315 <72307884+Aanchal315@users.noreply.github.com>
2020-12-28 10:34:30 +0530

    Changes in controller for swagger documentation

--- src/main/java/com/sce/controller/CurrencyConverterController.java
+++ src/main/java/com/sce/controller/CurrencyConverterController.java
@@ -8,14 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sce.service.CurrencyConverterService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/currency-converter-application")
+@Api(tags = "Currency Converter API", description = "API meant for Currency Conversion")
 public class CurrencyConverterController {
 	@Autowired
 	CurrencyConverterService currencyConverterService;
 
-	@GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
-	public double convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable float quantity) {
+	@ApiOperation(value = "Convert a Currency")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Returns the converted currency successfully."),
+			@ApiResponse(code = 400, message = "Returns the error message when invalid path parameters are passed.") })
+	@GetMapping(value = "/from/{from}/to/{to}/quantity/{quantity}")
+	public double convertCurrency(
+			@ApiParam(allowableValues = "INR, USD, EUR, GBP", defaultValue = "USD", required = true) @PathVariable String from,
+			@ApiParam(allowableValues = "INR, USD, EUR, GBP", defaultValue = "INR", required = true) @PathVariable String to,
+			@ApiParam(defaultValue = "1", required = true) @PathVariable double quantity) {
 		return currencyConverterService.convertCurrency(from, to, quantity);
 	}
 
